0ca7a2426c594d0a58748a988dcadae931d024b0
adlerzY <286251525+adlerzY@users.noreply.github.com>
2026-09-11 14:17:27 +0330

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,15 @@ const nextConfig = {
   poweredByHeader: false,
 
   images: {
-    unoptimized: process.env.NODE_ENV !== 'production',
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'api.arena2battle.com',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'arena2battle.com',
-        pathname: '/**',
       },
     ],
   },
