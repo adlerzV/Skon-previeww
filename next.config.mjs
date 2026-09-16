600053a99428bc897309b75860a1dec184b6ee27
adlerzY <286251525+adlerzY@users.noreply.github.com>
2026-09-17 03:13:37 +0330

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,39 @@ const nextConfig = {
 
   images: {
     formats: ['image/webp'],
-    deviceSizes: [360, 414, 640, 750, 828, 1080, 1200, 1600, 1920],
-    imageSizes: [16, 24, 32, 48, 64, 86, 96, 108, 128, 256, 384],
+
+    deviceSizes: [
+      360,
+      414,
+      640,
+      750,
+      828,
+      1080,
+      1200,
+      1600,
+      1920,
+    ],
+
+    imageSizes: [
+      16,
+      24,
+      32,
+      48,
+      64,
+      86,
+      96,
+      108,
+      128,
+      256,
+      384,
+    ],
+
     qualities: [60, 70, 75, 80, 85, 90],
+
     minimumCacheTTL: 60 * 60 * 24 * 30,
+
+    dangerouslyAllowLocalIP: true,
+
     remotePatterns: [
       {
         protocol: 'https',
